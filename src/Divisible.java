9276de8e93f2ff1a77e294d1494f67265dc7577c
Allen Payne <Allen Payne@DESKTOP-88ABTDK>
2018-09-18 23:48:52 -0400

    There was a logic error in the counter if-statement. The correct number to check for was 19, not 18. Nonetheless, the problem is now solved.

--- src/Divisible.java
+++ src/Divisible.java
@@ -5,12 +5,12 @@ public class Divisible {
 		while(true) {
 			int counter = 0;
 			for(int y = 2; y < 21; y++) { //starting at 2 again
-				counter++; // I want to count the amount of times this loops, if it hits 18, the number is divisible by 1->20
+				counter++; // I want to count the amount of times this loops, if it hits 19, the number is divisible by 1->20
 				if(x % y != 0) { // if x is divisible by y, I don't want this number
 					break; //ends the for loop to stop extra loops (also makes the counter work correctly)
 				}
 			}
-			if(counter == 18) { //if the counter is 18, the number is divisible by 1-20
+			if(counter == 19) { //if the counter is 19, the number is divisible by 1-20
 				System.out.println(x); //gives the number 
 				break; // once I find the number, I want the loop to terminate
 			}
